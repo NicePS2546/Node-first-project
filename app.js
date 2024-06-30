cef4f619dbe55b0514bc5fde69a1e385e156a239
nicelnwza <maoezxooo@gmail.com>
2024-06-30 18:48:18 +0700

    fix file

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 import { request } from 'http';
 import { display } from './logger.js';
-// import path from 'path'
 import express, { response } from 'express';
 import { readFile } from 'fs';
 import dotenv from 'dotenv';
-import path from 'path';
+
 
 
 dotenv.config();
 const app = express();
 
-var pathObj = path.parse(__filename);
+
 
 
 app.listen(process.env.NODE_PORT,()=>{
     console.log(`app open at Port  ${process.env.NODE_PORT}`);
     console.log(process.env.NODE_PUBLIC_URL);
-    console.log(pathObj);
+    
 })
 
 app.get('/',(request,response)=>{
